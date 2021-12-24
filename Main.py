bc0667ab4d66e5a965f1b2b8eff0d5f3f435dd9a
7EFF <90554401+7EFF@users.noreply.github.com>
2021-12-24 19:20:30 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -17,7 +17,7 @@ from pygame.locals import (
 
 gameBoard = [
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,],
-    [0,8,1,1,1,1,0,0,0,0,0,0,0,0,1,1,1,8,1,0,],
+    [0,2,1,1,1,1,0,0,0,0,0,0,0,0,1,1,1,8,1,0,],
     [0,1,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,1,0,],
     [0,1,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,1,0,],
     [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,],
@@ -33,24 +33,36 @@ gameBoard = [
     [0,0,0,0,0,1,0,1,0,0,0,0,1,0,1,0,0,0,0,0,],
     [0,1,8,1,1,1,0,1,1,0,0,1,1,0,1,1,1,1,1,0,],
     [0,1,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,1,0,],
-    [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,],
+    [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,8,0,],
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,],
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,]
 ]
 [length,width] =[len(gameBoard[0])*square,len(gameBoard[1])*square]
 screen = pygame.display.set_mode((width, length))
 pygame.display.flip()
-enemiesSafeArea=[6,6]
-#enemies=[Ghost(7,6),Ghost(7,7),Ghost(7,8)]
 pacman=[1,1]
 lives=1
 
 
-def move(row,col):
+def canMove(row,col):
     if gameBoard[int(row)][int(col)]!=0:
         return True
     return False
 
+
+def move(direction,pacman):
+    if direction == 'up':
+        if canMove(math.floor(pacman[0] - .25), pacman[1]) and pacman[1] % 1.0 == 0:
+            pacman[0] -= .25
+    if direction == 'right':
+        if canMove(pacman[0], math.ceil(pacman[1] + .25)) and pacman[0] % 1.0 == 0:
+            pacman[1] += .25
+    if direction == 'left':
+        if canMove(pacman[0], math.floor(pacman[1] - .25)) and pacman[0] % 1.0 == 0:
+            pacman[1] -= .25
+    if direction == 'down':
+        if canMove(math.ceil(pacman[0] + .25), pacman[1]) and pacman[1] % 1.0 == 0:
+            pacman[0] += .25
 
 def Board():
     screen.fill((0,0,0))
@@ -66,14 +78,13 @@ def Board():
                 pygame.draw.circle(screen, [100,0,150], (j * square + square/2, i * square + square/2),square/4)
             elif gameBoard[i][j] == 8:
                 pygame.draw.circle(screen, [204, 102, 0], (j * square + square/2, i * square + square/2),square/5)
-    '''for enemie in enemies:
-        pygame.draw.circle(screen,(255,0,0),enemie.col * square+square//2,enemie.row*square+square//2)'''
     pygame.draw.circle(screen,[255,255,0],(math.floor(pacman[1]*square)+square/2,math.floor(pacman[0]*square+square/2)),square/3)
     pygame.display.update()
 
 direction ='up'
 running=True
 coinCount=0
+req='up'
 
 while running:
     Board()
@@ -82,30 +93,31 @@ while running:
             if event.key == K_ESCAPE:
                 running = False
             elif event.key == K_UP:
-                if (move(math.floor(pacman[0] - .25), pacman[1]) and pacman[1]%1.0 ==0):
-                    direction='up'
+                req='up'
             elif event.key == K_RIGHT:
-                if (move(pacman[0], math.ceil(pacman[1] + .25)) and pacman[0]%1.0 ==0):
-                    direction='right'
+                req='right'
             elif event.key == K_LEFT:
-                if (move(pacman[0], math.floor(pacman[1] - .25)) and pacman[0]%1.0 ==0):
-                    direction='left'
+                req='left'
             elif event.key == K_DOWN:
-                if (move(math.ceil(pacman[0] + .25), pacman[1]) and pacman[1]%1.0 ==0):
-                    direction='down'
+                req='down'
 
-    if direction=='up':
-        if (move(math.floor(pacman[0] - .25), pacman[1]) and pacman[1]%1.0 ==0):
-            pacman[0] -= .25
-    if direction=='right':
-        if (move(pacman[0], math.ceil(pacman[1] + .25)) and pacman[0]%1.0 ==0):
-            pacman[1] += .25
-    if direction=='left':
-        if (move(pacman[0], math.floor(pacman[1] - .25)) and pacman[0]%1.0 ==0):
-            pacman[1] -= .25
-    if direction=='down':
-        if (move(math.ceil(pacman[0] + .25), pacman[1]) and pacman[1]%1.0 ==0):
-            pacman[0] += .25
+    if req=='up':
+        if (canMove(math.floor(pacman[0] - .25), pacman[1]) and pacman[1]%1.0 ==0):
+            direction='up'
+        move(direction,pacman)
+    if req=='right':
+        if (canMove(pacman[0], math.ceil(pacman[1] + .25)) and pacman[0]%1.0 ==0):
+            direction='right'
+        move(direction, pacman)
+    if req=='left':
+        if (canMove(pacman[0], math.floor(pacman[1] - .25)) and pacman[0]%1.0 ==0):
+            direction='left'
+        move(direction,pacman)
+    if req=='down':
+        if (canMove(math.ceil(pacman[0] + .25), pacman[1]) and pacman[1]%1.0 ==0):
+            direction='down'
+        move(direction, pacman)
+
     if gameBoard[int(pacman[0])][int(pacman[1])] == 1:
         coinCount += 10
         print("The current coins:", coinCount)
