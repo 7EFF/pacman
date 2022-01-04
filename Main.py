97f93efb763b878c8971a358152d670f1881c323
7EFF <90554401+7EFF@users.noreply.github.com>
2022-01-04 10:53:03 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -10,7 +10,7 @@ pygame.mixer.init()
 from ghosts_class import *
 
 class PacMan:
-    def __init__(self,direction,gameBoard,square,screen,pacman,coinCount,length,width):
+    def __init__(self,direction,gameBoard,square,screen,pacman,coinCount,length,width,pacspeed):
         self.direction=direction
         self.gameBoard = gameBoard
         self.square=square
@@ -24,6 +24,7 @@ class PacMan:
         self.g_pos=[]
         self.make_ghosts()
         self.colour=0
+        self.pacspeed=pacspeed
 
     ###########################MOVEMENT###########################
 
@@ -35,17 +36,17 @@ class PacMan:
 
     def move(self,direction,row,col):
         if direction == 'up':
-            if self.canMove(math.floor(row - .25), col) and col % 1.0 == 0:
-                row -= .25
+            if self.canMove(math.floor(row - self.pacspeed), col) and col % 1.0 == 0:
+                row -= self.pacspeed
         if direction == 'right':
-            if self.canMove(row, math.ceil(col + .25)) and row % 1.0 == 0:
-                col += .25
+            if self.canMove(row, math.ceil(col + self.pacspeed)) and row % 1.0 == 0:
+                col += self.pacspeed
         if direction == 'left':
-            if self.canMove(row, math.floor(col - .25)) and row % 1.0 == 0:
-                col -= .25
+            if self.canMove(row, math.floor(col - self.pacspeed)) and row % 1.0 == 0:
+                col -= self.pacspeed
         if direction == 'down':
-            if self.canMove(math.ceil(row + .25), col) and col % 1.0 == 0:
-                row += .25
+            if self.canMove(math.ceil(row + self.pacspeed), col) and col % 1.0 == 0:
+                row += self.pacspeed
         return row,col
 
     ###########################MOVEMENT###########################
@@ -79,14 +80,13 @@ class PacMan:
                     pygame.draw.rect(self.screen,[15,0,50],(j*self.square, i*self.square,self.square,self.square),math.floor(self.square/2))
                     pygame.draw.rect(self.screen, [15, 0, 50],(j * self.square, i * self.square, int(self.square/1.5), int(self.square/1.5)))
                 elif self.gameBoard[i][j] == 1:
-                    pygame.draw.circle(self.screen, [255, 255, 255], (j * self.square + self.square/2, i * self.square + self.square/2), self.square/8,int(self.square/20))
+                    pygame.draw.circle(self.screen, [255, 255, 255], (j * self.square + self.square/2, i * self.square + self.square/2), self.square/15)
                 if self.gameBoard[i][j]==2:
                     pygame.draw.circle(self.screen, [0, 0, 0], (j * self.square + self.square / 2, i * self.square + self.square / 2), self.square / 5)
                 elif self.gameBoard[i][j] == 3:
                     pygame.draw.circle(self.screen, [204, 102, 0], (j * self.square + self.square/2, i * self.square + self.square/2),self.square/5)
 
                 if self.gameBoard[i][j] == 4 or self.gameBoard[i][j] == 5 or self.gameBoard[i][j] == 6 or self.gameBoard[i][j] == 7:
-                    #pygame.draw.circle(self.screen, [100, 0, 200], (j * self.square + self.square/2, i * self.square + self.square/2),self.square/4)
                     self.g_pos.append([i, j])
         pygame.draw.circle(self.screen,[255,255,0],(math.floor(self.pacman[1]*self.square+self.square/2),math.floor(self.pacman[0]*self.square+self.square/2)),self.square/3)
         Font = pygame.font.SysFont('arial black', math.floor(self.square/2))
@@ -175,8 +175,19 @@ class PacMan:
             pygame.display.update()
 
     ###########################DRAWING###########################
+
+    ###########################HELPER###########################
+
+    def pause(self,time):
+        cur = 0
+        while not cur == time:
+            cur += 1
+
+    ###########################HELPER###########################
+
 def main():
     square = 35
+    pacspeed=1/8
     from pygame.locals import (
         K_UP,
         K_DOWN,
@@ -218,14 +229,13 @@ def main():
     screen = pygame.display.set_mode((width, length))
     pygame.display.flip()
     pacman = [1, 1]
-    lives = 1
     pygame.display.set_caption("PacMan-Final Project")
     direction = 'up'
     running = True
     coinCount = 0
     req = 'up'
-    user = PacMan(direction, gameBoard, square, screen, pacman,coinCount,length,width)
-    #user.Intro_Render()
+    user = PacMan(direction, gameBoard, square, screen, pacman,coinCount,length,width,pacspeed)
+    user.Intro_Render()
     while running:
         user.Board()
         for event in pygame.event.get():
@@ -245,19 +255,19 @@ def main():
                 sys.exit()
 
         if req=='up':
-            if (user.canMove(math.floor(pacman[0] - .25), pacman[1]) and pacman[1]%1.0 ==0):
+            if (user.canMove(math.floor(pacman[0] - pacspeed), pacman[1]) and pacman[1]%1.0 ==0):
                 direction='up'
             pacman[0],pacman[1]=user.move(direction, pacman[0],pacman[1])
         if req=='right':
-            if (user.canMove(pacman[0], math.ceil(pacman[1] + .25)) and pacman[0]%1.0 ==0):
+            if (user.canMove(pacman[0], math.ceil(pacman[1] + pacspeed)) and pacman[0]%1.0 ==0):
                 direction='right'
             pacman[0],pacman[1]=user.move(direction, pacman[0],pacman[1])
         if req=='left':
-            if (user.canMove(pacman[0], math.floor(pacman[1] - .25)) and pacman[0]%1.0 ==0):
+            if (user.canMove(pacman[0], math.floor(pacman[1] - pacspeed)) and pacman[0]%1.0 ==0):
                 direction='left'
             pacman[0],pacman[1]=user.move(direction, pacman[0],pacman[1])
         if req=='down':
-            if (user.canMove(math.ceil(pacman[0] + .25), pacman[1]) and pacman[1]%1.0 ==0):
+            if (user.canMove(math.ceil(pacman[0] + pacspeed), pacman[1]) and pacman[1]%1.0 ==0):
                 direction='down'
             pacman[0],pacman[1]=user.move(direction, pacman[0],pacman[1])
 
@@ -265,8 +275,7 @@ def main():
             coinCount += 10
             print("The current coins:", coinCount)
             gameBoard[int(pacman[0])][int(pacman[1])] = 2
-            sound_delay = 40
-            if coinCount%sound_delay==10:
+            if pygame.mixer.get_busy()==False:
                 eating_Sound= mixer.Sound('pacman_chomp.wav')
                 eating_Sound.set_volume(0.2)
                 eating_Sound.play()
@@ -281,7 +290,7 @@ def main():
             user.died_wait()
         if coinCount==1900:
             user.winning()
-        time.sleep(0.03)
-        user = PacMan(direction, gameBoard, square, screen, pacman,coinCount,length,width)
+        user.pause(100000)
+        user = PacMan(direction, gameBoard, square, screen, pacman,coinCount,length,width,pacspeed)
 if __name__ == '__main__':
     main()
