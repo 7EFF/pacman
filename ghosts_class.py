5efa05172673904d1c1ebda04f1ac3cc4fed046b
7EFF <90554401+7EFF@users.noreply.github.com>
2022-03-01 11:04:16 +0200

    Update ghosts_class.py

--- ghosts_class.py
+++ ghosts_class.py
@@ -1,88 +1,147 @@
 import pygame
 import math
 import random
+
 pygame.init()
 pygame.font.init()
 pygame.mixer.init()
 
+
 class Ghost:
-    def __init__(self,main,row,col,colour,pacmanRow,pacmanCol):
-        self.main=main
-        self.row=row
-        self.col=col
-        self.colour=colour
+    def __init__(self, main, row, col, colour, pacmanRow, pacmanCol):
+        self.main = main
+        self.row = row
+        self.col = col
+        self.colour = colour
         self.pacmanRow = pacmanRow
-        self.pacmanCol=pacmanCol
-        self.ogrow=row
-        self.ogcol=col
+        self.pacmanCol = pacmanCol
+        self.ogrow = row
+        self.ogcol = col
         self.ogcolour = colour
-        self.ghostspeed=1/128
-        self.ghostBehave='Leave'
-        self.direction='up'
-        self.died=False
-        self.leftSpawn=False
+        self.ghostspeed = 1 / 128
+        self.ghostBehave = 'Leave'
+        self.direction = 'up'
+        self.died = False
+        self.leftSpawn = False
 
     def ifTouched(self):
-        if math.floor(self.row)==math.floor(self.main.pacman[0]) and math.ceil(self.main.pacman[1])==math.ceil(self.col) and self.main.eatGhosts==False:
+        if math.floor(self.row) == math.floor(self.main.pacman[0]) and math.ceil(self.main.pacman[1]) == math.ceil(
+                self.col) and self.main.eatGhosts == False:
             return 'died'
-        if math.floor(self.row)==math.floor(self.main.pacman[0]) and math.ceil(self.main.pacman[1])==math.ceil(self.col) and self.main.eatGhosts==True and self.died==False:
+        if math.floor(self.row) == math.floor(self.main.pacman[0]) and math.ceil(self.main.pacman[1]) == math.ceil(
+                self.col) and self.main.eatGhosts == True and self.died == False:
             return 'eaten'
-        if math.floor(self.row) == math.floor(self.main.pacman[0]) and math.ceil(self.main.pacman[1]) == math.ceil(self.col) and self.died==True:
+        if math.floor(self.row) == math.floor(self.main.pacman[0]) and math.ceil(self.main.pacman[1]) == math.ceil(
+                self.col) and self.died == True:
             return 'died'
 
-    def canMove(self,row,col,index):
-        if self.main.gameBoard[int(row)][int(col)]!=index and self.main.gameBoard[int(row)][int(col)]!=0:
+    def canMove(self, row, col, index):
+        if self.main.gameBoard[int(row)][int(col)] != index and self.main.gameBoard[int(row)][int(col)] != 0:
             return True
         return False
 
-    def changeBehavior(self,NewBehave):
-        self.ghostBehave=NewBehave
+    def changeBehavior(self, NewBehave):
+        self.ghostBehave = NewBehave
+
+    def chooseDir(self, possibleTurn):
+        index = 0
+        if self.died:
+            index = 4
+        r_difference = self.row - self.main.pacman[0]
+        c_difference = self.col - self.main.pacman[1]
+
+        if 'down' in possibleTurn and r_difference<0:
+            if self.canMove(math.ceil(self.row + self.ghostspeed), self.col,index) and self.col % 1.0 == 0 and self.direction != 'up':
+                self.direction='down'
+                return
+        if 'up' in possibleTurn and r_difference>0:
+            if self.canMove(math.floor(self.row - self.ghostspeed), self.col,index) and self.col % 1.0 == 0 and self.direction != 'down':
+                self.direction='up'
+                return
+        if 'right' in possibleTurn and c_difference<0:
+            if self.canMove(self.row, math.ceil(self.col + self.ghostspeed),index) and self.row % 1.0 == 0 and self.direction != 'left':
+                self.direction='right'
+                return
+        if 'left' in possibleTurn and c_difference>0:
+            if self.canMove(self.row, math.floor(self.col - self.ghostspeed),index) and self.row % 1.0 == 0 and self.direction != 'right':
+                self.direction='left'
+                return
+
+
+    def Hunter(self):
+        index = 0
+        if self.died:
+            index = 4
+        possibleTurn = []
+        if self.canMove(self.row, math.floor(self.col - self.ghostspeed),
+                        index) and self.row % 1.0 == 0 and self.direction != 'right':
+            possibleTurn.append('left')
+        if self.canMove(math.ceil(self.row + self.ghostspeed), self.col,
+                        index) and self.col % 1.0 == 0 and self.direction != 'up':
+            possibleTurn.append('down')
+        if self.canMove(math.floor(self.row - self.ghostspeed), self.col,
+                        index) and self.col % 1.0 == 0 and self.direction != 'down':
+            possibleTurn.append('up')
+        if self.canMove(self.row, math.ceil(self.col + self.ghostspeed),
+                        index) and self.row % 1.0 == 0 and self.direction != 'left':
+            possibleTurn.append('right')
+
+        if len(possibleTurn) >= 2:
+            self.chooseDir(possibleTurn)
+        elif len(possibleTurn) == 1:
+            self.direction = possibleTurn[0]
+        self.move()
 
     def randDirection(self):
         index = 0
         if self.died:
             index = 4
-        possibleTurn=[]
-        if self.canMove(math.floor(self.row - self.ghostspeed), self.col,index) and self.col % 1.0 == 0 and self.direction!='down':
+        possibleTurn = []
+        if self.canMove(math.floor(self.row - self.ghostspeed), self.col,
+                        index) and self.col % 1.0 == 0 and self.direction != 'down':
             possibleTurn.append('up')
-        if self.canMove(self.row, math.ceil(self.col + self.ghostspeed),index) and self.row % 1.0 == 0 and self.direction!='left':
+        if self.canMove(self.row, math.ceil(self.col + self.ghostspeed),
+                        index) and self.row % 1.0 == 0 and self.direction != 'left':
             possibleTurn.append('right')
-        if self.canMove(self.row, math.floor(self.col - self.ghostspeed),index) and self.row % 1.0 == 0 and self.direction!='right':
+        if self.canMove(self.row, math.floor(self.col - self.ghostspeed),
+                        index) and self.row % 1.0 == 0 and self.direction != 'right':
             possibleTurn.append('left')
-        if self.canMove(math.ceil(self.row + self.ghostspeed), self.col,index) and self.col % 1.0 == 0 and self.direction!='up':
+        if self.canMove(math.ceil(self.row + self.ghostspeed), self.col,
+                        index) and self.col % 1.0 == 0 and self.direction != 'up':
             possibleTurn.append('down')
-        if len(possibleTurn)>=1:
-            self.direction=random.choice(possibleTurn)
+        self.direction = random.choice(possibleTurn)
         self.move()
 
     def movementBehaves(self):
-        if self.ghostBehave=='Random':
+        if self.ghostBehave == 'Random':
             self.randDirection()
-        if self.ghostBehave=='Leave':
+        if self.ghostBehave == 'Hunt':
+            self.Hunter()
+        if self.ghostBehave == 'Leave':
             self.move()
-            if self.main.gameBoard[int(self.row)][int(self.col)]==5:
-                self.leftSpawn=True
+            if self.main.gameBoard[round(self.row)][round(self.col)] == 5:
+                self.leftSpawn = True
                 self.changeBehavior('Random')
 
     def move(self):
-        index =0
+        index = 0
         if self.died or self.leftSpawn:
-            index=4
+            index = 4
         if self.direction == 'up':
-            if self.canMove(math.floor(self.row - self.ghostspeed), self.col,index) and self.col % 1.0 == 0:
+            if self.canMove(math.floor(self.row - self.ghostspeed), self.col, index) and self.col % 1.0 == 0:
                 self.row -= self.ghostspeed
         elif self.direction == 'right':
-            if self.canMove(self.row, math.ceil(self.col + self.ghostspeed),index) and self.row % 1.0 == 0:
+            if self.canMove(self.row, math.ceil(self.col + self.ghostspeed), index) and self.row % 1.0 == 0:
                 self.col += self.ghostspeed
         elif self.direction == 'left':
-            if self.canMove(self.row, math.floor(self.col - self.ghostspeed),index) and self.row % 1.0 == 0:
-                self. col -= self.ghostspeed
+            if self.canMove(self.row, math.floor(self.col - self.ghostspeed), index) and self.row % 1.0 == 0:
+                self.col -= self.ghostspeed
         elif self.direction == 'down':
-            if self.canMove(math.ceil(self.row + self.ghostspeed), self.col,index) and self.col % 1.0 == 0:
-                self. row += self.ghostspeed
-        if self.col<0.015625 and self.direction=='left':
+            if self.canMove(math.ceil(self.row + self.ghostspeed), self.col, index) and self.col % 1.0 == 0:
+                self.row += self.ghostspeed
+        if self.col < 0.015625 and self.direction == 'left':
             self.col = 27.484375
-        if self.col>26.984375 and self.direction=='right':
+        if self.col > 26.984375 and self.direction == 'right':
             self.col = 0.015625
         return
 
@@ -92,17 +151,18 @@ class Ghost:
     def bigCoinEaten(self):
         self.colour = 'blue'
         self.movementBehaves()
-        self.ghostspeed=1/256
-        self.died=False
+        self.ghostspeed = 1 / 256
+        self.died = False
 
     def eatenBlue(self):
         self.row = self.ogrow
         self.col = self.ogcol
         self.colour = self.ogcolour
-        self.ghostspeed = 1 / 128
-        self.died=True
+        self.ghostspeed = 1 / 2
+        self.changeBehavior('Random')
+        self.died = True
         self.leftSpawn = False
-        self.direction='up'
+        self.direction = 'up'
 
     def flickerToOG(self):
         self.colour = self.ogcolour
@@ -116,10 +176,10 @@ class Ghost:
     def blueOver(self):
         self.colour = self.ogcolour
         self.ghostspeed = 1 / 128
-        self.died=False
-        if self.leftSpawn==False:
-            self.direction='up'
-            self.ghostBehave='Leave'
+        self.died = False
+        if self.leftSpawn == False:
+            self.direction = 'up'
+            self.ghostBehave = 'Leave'
             self.movementBehaves()
             self.row = self.ogrow
             self.col = self.ogcol
@@ -135,26 +195,28 @@ class Ghost:
             return 'left.png'
 
     def drawGhost(self):
-        Directory=''
-        if self.colour=='blue':
+        Directory = ''
+        if self.colour == 'blue':
             Directory = 'GhostsPics\_blue.png'
         else:
-            if self.colour=='pink':
-                Directory='GhostsPics\_pink_'
-                Dir=self.DirForPic()
-                Directory = Directory+Dir
-            elif self.colour=='cyan':
+            if self.colour == 'pink':
+                Directory = 'GhostsPics\_pink_'
+                Dir = self.DirForPic()
+                Directory = Directory + Dir
+            elif self.colour == 'cyan':
                 Directory = 'GhostsPics\_cyan_'
                 Dir = self.DirForPic()
                 Directory = Directory + Dir
-            elif self.colour=='yellow':
+            elif self.colour == 'yellow':
                 Directory = 'GhostsPics\_yellow_'
                 Dir = self.DirForPic()
                 Directory = Directory + Dir
-            elif self.colour=='red':
-                Directory='GhostsPics\_red_'
+            elif self.colour == 'red':
+                Directory = 'GhostsPics\_red_'
                 Dir = self.DirForPic()
                 Directory = Directory + Dir
         ghost_Pic = pygame.image.load(Directory).convert()
         ghost_Pic = pygame.transform.scale(ghost_Pic, (int(self.main.square * 1.3), int(self.main.square * 1.3)))
-        self.main.screen.blit(ghost_Pic, (math.floor(self.col * self.main.square), math.floor(self.row * self.main.square), self.main.square, self.main.square))
+        self.main.screen.blit(ghost_Pic, (
+        math.floor(self.col * self.main.square), math.floor(self.row * self.main.square), self.main.square,
+        self.main.square))
