684eb9bce3cad4c325f85afc3f1fb2388d9ed812
7EFF <90554401+7EFF@users.noreply.github.com>
2022-03-22 11:06:07 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import select
+import time
 
 MAX_MSG_LENGTH = 1024
 SERVER_PORT = 5555
@@ -11,8 +12,34 @@ server_socket.bind((SERVER_IP, SERVER_PORT))
 server_socket.listen()
 print("Listening for clients...")
 client_sockets = []
-Coins_Results=[]
-Times_Results=[]
+Coins_Results={}
+Times_Results={}
+
+class server:
+    def __init__(self,client_sockets,current_socket,Coins_Results,Times_Results):
+        self.client_sockets = client_sockets
+        self.current_socket = current_socket
+        self.Coins_Results=Coins_Results
+        self.Times_Results=Times_Results
+
+    def checkWinner(self):
+        maxCoins=0
+        maxCoinsClient=""
+        switchToTime=False
+        for i in client_sockets:
+            if Coins_Results[i]>maxCoins:
+                maxCoins=Coins_Results[i]
+                maxCoinsClient=i
+                switchToTime = False
+            if Coins_Results[i]==maxCoins:
+                switchToTime=True
+        if switchToTime:
+            maxTime=0
+            maxCoinsTime=""
+            for i in client_sockets:
+                if Times_Results[i] > maxCoins:
+                    maxTime = Times_Results[i]
+                    maxCoinsTime = i
 
 def main():
     while True:
@@ -23,12 +50,23 @@ def main():
                 print("New client joined!", client_address)
                 client_sockets.append(connection)
             else:
-                Result_Coins=current_socket.recv(MAX_MSG_LENGTH).decode()  # כמה מטבעות קיבל הלקוח
-                if Result_Coins!=0:
+                Result_Coins=current_socket.recv(MAX_MSG_LENGTH).decode() # כמה מטבעות קיבל הלקוח
+                if Result_Coins=="":
+                    client_sockets.remove(current_socket)
+                    current_socket.close()
+                    continue
+                else:
                     Coins_Results[current_socket] = Result_Coins
+                    print(Result_Coins, "Coins")
                 Result_Time=current_socket.recv(MAX_MSG_LENGTH).decode() #כמה זמן סיים הלקוח
-                if Result_Time != 0:
-                    Times_Results[current_socket]=Result_Time
+                if Result_Time == "":
+                    client_sockets.remove(current_socket)
+                    current_socket.close()
+                    continue
+                else:
+                    Times_Results[current_socket] = Result_Time
+                    print(Result_Time, "Time")
+                Game = server(client_sockets, current_socket, Coins_Results,Times_Results)
 
 if __name__ == '__main__':
     main()
