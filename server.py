7a3d8b03012b91bff1533a5ac0c87bbf43e9c8ce
7EFF <90554401+7EFF@users.noreply.github.com>
2022-05-10 14:39:10 +0300

    Update server.py

--- server.py
+++ server.py
@@ -20,11 +20,11 @@ Times_Results = {}
 
 class server:
 
-    def __init__(self, client_sockets, current_socket, Coins_Results, Times_Results, Recieved_Clients):
+    def __init__(self, client_sockets, current_socket, Recieved_Clients):
         self.client_sockets = client_sockets
         self.current_socket = current_socket
-        self.Coins_Results = Coins_Results
-        self.Times_Results = Times_Results
+        self.Coins_Results = {}
+        self.Times_Results = {}
         self.Recieved_Clients=Recieved_Clients
 
     def get_Recieved_Clients(self):
@@ -42,39 +42,41 @@ class server:
                 client_sockets.remove(c)
                 c.close()
                 continue
-            else:
-                Coins_Results[c] = Result_Coins
-                print(Result_Coins, "Coins")
-            Times_Results[c] = Result_Time
+            self.Coins_Results[c] = Result_Coins
+            print(Result_Coins, "Coins")
+            self.Times_Results[c] = Result_Time
             print(Result_Time, "Time")
             self.Recieved_Clients[c] = True #מסמן שאותו לקוח שלח את כל מה שהיה צריך לשלוח
+
+        self.checkWinner()  # בודק מי ניצח באחד המשחקים
         return
 
     def checkWinner(self):
         maxCoins = 0
+        minTime=0
         maxCoinsClient = ""
         winner = ""
         switchToTime = False
-        for i in client_sockets:
-            if int(Coins_Results[i]) > maxCoins:
-                maxCoins = int(Coins_Results[i])
+        for i in self.client_sockets:
+            if int(self.Coins_Results[i]) > maxCoins:
+                maxCoins = int(self.Coins_Results[i])
                 maxCoinsClient = i
                 switchToTime = False
                 winner = i
-            if Coins_Results[i] == maxCoins:
+            if self.Coins_Results[i] == maxCoins:
                 switchToTime = True
         print(maxCoins, "Max Coins")
         if switchToTime:
             minTime = 0
             minTimeClient = ""
-            for i in client_sockets:
-                if int(Times_Results[i]) < minTime:
-                    minTime = Times_Results[i]
+            for i in self.client_sockets:
+                if int(self.Times_Results[i]) < minTime:
+                    minTime = self.Times_Results[i]
                     minTimeClient = i
-                print(minTime)
                 winner = i
+        print(minTime, "Min Time")
 
-        for c in client_sockets:
+        for c in self.client_sockets:
             if c == winner:
                 msg = "You have won !"
                 c.send(msg.encode())
@@ -87,29 +89,30 @@ def main():
     Recieved_Clients = {}
     Number_Clients=0
     Waiting_Room=[]
+    Game_list=[]
     while 1:
         rlist, wlist, xlist = select.select([server_socket] + client_sockets, client_sockets, [])
         for current_socket in rlist:
             if current_socket is server_socket:
                 connection, client_address = current_socket.accept()
                 print("New client joined!", client_address)
-                Request = connection.recv(MAX_MSG_LENGTH).decode()#מקבל את הבקשה מאחד הלקוחות. שחקן, צופה או לשחק עוד הפעם
+                Request = connection.recv(MAX_MSG_LENGTH).decode()  # מקבל את הבקשה מאחד הלקוחות. שחקן, צופה או לשחק עוד הפעם
                 if Request=="go":
                     client_sockets.append(connection)
                     Waiting_Room.append(connection)
                     Recieved_Clients[connection] = False
-                    if len(Waiting_Room) == 2:
-                        Lobby = server(Waiting_Room, current_socket, Coins_Results, Times_Results,Recieved_Clients)#יוצר את המשחק הכללי
+                    if len(Waiting_Room) >= 2:
                         for c in Waiting_Room:
                             msg = "You can start"
                             c.send(msg.encode()) #שולח לכל שחקן שהוא יכול להתחיל לשחק
-                        x = threading.Thread(target=Lobby.Game()) #יוצר משחק בין שני אנשים
-                        x.start()
-                        Waiting_Room.clear()
-                        x.join()
-                        #print(Lobby.Recieved_Clients.values())
-                        if not False in Lobby.Recieved_Clients.values():
-                            Lobby.checkWinner()#בודק מי ניצח באחד המשחקים
+                        Players=int(len(Waiting_Room)/2)
+                        for i in range(Players):
+                            Game_list = Waiting_Room[0],Waiting_Room[1]
+                            Lobby = server(Game_list, current_socket, Recieved_Clients)  # יוצר את המשחק הכללי
+                            x = threading.Thread(target=Lobby.Game) #יוצר משחק בין שני אנשים
+                            x.start()
+                            Waiting_Room.remove(Waiting_Room[1])
+                            Waiting_Room.remove(Waiting_Room[0])
 
 
 if __name__ == '__main__':
